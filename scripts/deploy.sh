6400b828e33100a30ac9ac41736606c5946a39b6
insung2 <84179188+2insung@users.noreply.github.com>
2023-09-04 14:35:55 +0900

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,44 +4,44 @@
 # 1. env variable
 source ./projectVariable.sh
 source ./prodVariable.sh
-echo "1. env variable setting complete"
+echo "1. env variable setting complete" >> ${HOME}/deploy.log
 
 
 # 2. cron delete
 touch crontab_delete
 crontab crontab_delete
 rm crontab_delete
-echo "2. cron delete complete"
+echo "2. cron delete complete" >> ${HOME}/deploy.log
 
 # 3. server checking
 if [ -n "${PROJECT_PID}" ]; then
         kill -9 ${PROJECT_PID}
-        echo "3. project kill complete"
+        echo "3. project kill complete" >> ${HOME}/deploy.log
 else
         sudo yum -y update 1>/dev/null
-        echo "3-1. yum update complete"
+        echo "3-1. yum update complete" >> ${HOME}/deploy.log
 
         sudo yum -y install java-11-amazon-corretto.x86_64
-        echo "3-2. jdk install complete"
+        echo "3-2. jdk install complete" >> ${HOME}/deploy.log
 
         sudo timedatectl set-timezone Asia/Seoul
-        echo "3-3. timezone setting complete"
+        echo "3-3. timezone setting complete" >> ${HOME}/deploy.log
 fi
 
 
 # 4. gradlew +x
 chmod u+x ${HOME}/${PROJECT_NAME}/gradlew
-echo "4. gradlew u+x complete"
+echo "4. gradlew u+x complete" >> ${HOME}/deploy.log
 
 # 5. build
 cd ${HOME}/${PROJECT_NAME}
 ./gradlew clean build
-echo "5. gradlew build complete"
+echo "5. gradlew build complete" >> ${HOME}/deploy.log
 
 
 # 6. start jar
 nohup java -jar -Dspring.profiles.active=prod ${JAR_PATH} 1>${HOME}/log.out 2>${HOME}/err.out &
-echo "6. start server complete"
+echo "6. start server complete" >> ${HOME}/deploy.log
 
 # 7. cron registration
 touch crontab_new
@@ -49,4 +49,4 @@ echo "* * * * * ${HOME}/${PROJECT_NAME}/scripts/check-and-restart.sh" 1>>crontab
 # register the others..
 crontab crontab_new
 rm crontab_new
-echo "7. registration complete"
+echo "7. registration complete" >> ${HOME}/deploy.log
